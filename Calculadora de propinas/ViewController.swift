563bb63fe2df89bd8f5fa3ff723a6b87457e265a
Fer <reikafer@gmail.com>
2022-08-19 08:47:47 -0700

    mensaje

--- Calculadora de propinas/ViewController.swift
+++ Calculadora de propinas/ViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var PpropinaS: UISlider!
+    @IBOutlet weak var pago: UITextField!
+    @IBOutlet weak var Ppropina: UILabel!
+    @IBOutlet weak var Tpropina: UILabel!
+    @IBOutlet weak var propina: UILabel!
+    
+    var UIpago = "$ 0.0"
+    var UIPpropina = "$ 0.0"
+    var UITpropina = "$ 0.0"
+    var UIpropina = "0%"
+    var UIPpropinaS = 0
+    var porcentaje = Float(0.0);
+    var Pporcentaje = "0.0"
+    var TTotal = "0.0"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    @IBAction func PpropinaS(_ sender: Any) {
+        UIpago = "\(pago.text!)"
+        UIPpropinaS = Int(PpropinaS.value)
+        UIPpropina = "\(UIPpropinaS)"
+        UIpropina = "\(UIPpropinaS)"
+        Ppropina.text = "$\(UIpropina)%"
+        porcentaje = Float(UIpago)! * Float(UIPpropinaS)/Float(100.0)
+        Pporcentaje = String(format: "%.2f", porcentaje)
+        propina.text = "$ \(Pporcentaje)"
+        UITpropina = "\(Float(UIpago)! + porcentaje)"
+        TTotal = String(format: "%.2f", UITpropina)
+        Tpropina.text = "$ \(TTotal)"
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
